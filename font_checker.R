da43a05eb216ff288a2a921fc0aa5249f0192c5a
Pashio84 <yuhki_yk613st730@yahoo.co.jp>
2019-01-31 18:48:30 +0900

    埋め込み判定部を空白スプリットで抽出するようにした

--- font_checker.R
+++ font_checker.R
@@ -1,13 +1,25 @@
+library(stringr)
+
 checkDir <- commandArgs(trailingOnly = T)[1]
+# checkDir <- "/run/user/1000/gvfs/smb-share:server=eggplant,share=assist,user=assist/work_files/庶務/卒研梗概集/2019/梗概集/矢島研"
 setwd(checkDir)
 fileNames <- list.files()
+fileNames <- fileNames[!dir.exists(fileNames)][grep(".*\\.pdf", fileNames)]
+
+cat("フォント埋め込みがされていないデータ一覧\n\n")
 
 invisible(lapply(fileNames, function(file) {
-  info <- system(paste("pdffonts", file, "| nkf -w"), intern = TRUE)
-  embNum <- as.integer(strsplit(system(paste("pdffonts", file, "| grep -o -b emb"), intern = TRUE), ":")[[1]][1]) + 1
-  nameLastNum <- as.integer(strsplit(system(paste("pdffonts", fileNames[1], "| grep -o -b type"), intern = TRUE), ":")[[1]][1]) - 1
+  info <- system(paste("pdffonts", file, "| nkf -w"), intern = TRUE)[-(1:2)]
+  infoSplit <- str_split(info, pattern = " ")
+  doubleByteChar <- apply(str_extract_all(info, pattern=".", simplify=TRUE), 1, function(rowData) grep("[^\x01-\x7E]", rowData))
 
-  emb <- gsub(" *$", "", substr(info, embNum, embNum + 2))
+  nameLastNum <- as.integer(strsplit(system(paste("pdffonts", fileNames[1], "| grep -o -b type"), intern = TRUE), ":")[[1]][1]) - 1
+  if(length(doubleByteChar) != 0) {
+    doubleByteName <- unlist(lapply(doubleByteChar, function(word) length(word[word < nameLastNum])))
+    nameLastNum <- rep(nameLastNum, length(info)) - doubleByteName
+  }
+
+  emb <- unlist(lapply(infoSplit, function(font) font[font == "yes" | font == "no"][1]))
   name <- gsub(" *$", "", substr(info, 1, nameLastNum))
 
   results <- name[emb == "no"]
